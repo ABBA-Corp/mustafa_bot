3bc533c6b3ffcc3de8be767e742fa058cfa074ab
bakhrullo <81313486+bakhrullo@users.noreply.github.com>
2023-02-04 22:30:48 +0300

    Update run.py

--- run.py
+++ run.py
@@ -2,6 +2,8 @@ import os
 import django
 from aiogram import Bot, Dispatcher
 import logging
+import sentry_sdk
+from data import config
 
 
 async def on_startup(dp):
@@ -29,6 +31,14 @@ def setup_django():
 
 if __name__ == '__main__':
     setup_django()
+     sentry_sdk.init(
+        dsn=config.SENTRY_DSN,
+
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production.
+        traces_sample_rate=1.0
+    )
 
     from aiogram.utils import executor
     from handlers import dp
