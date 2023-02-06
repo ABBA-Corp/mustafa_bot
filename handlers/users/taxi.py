41f24a7ffac61fe0d698a975b66285b17e582171
bakhrullo <81313486+bakhrullo@users.noreply.github.com>
2023-02-06 23:33:57 +0500

    bug fix

--- handlers/users/taxi.py
+++ handlers/users/taxi.py
@@ -70,9 +70,7 @@ async def create_order(user, order, longitude, latitude, address, comment):
                                 data=json.dumps(data)
                                 ) as response:
             res = await response.json()
-            logging.error("test")
-            logging.error(res)
-            logging.error("test2")
+
     async with aiohttp.ClientSession() as session:
         async with session.post(url=f"{config.TAXI}accept",
                                 headers={"Authorization": f"Bearer {config.TAXI_TOKEN}",
@@ -82,4 +80,4 @@ async def create_order(user, order, longitude, latitude, address, comment):
                                 data=json.dumps({"version": 1})
                                 ) as response:
             resps = await response.json()
-            print(resps)
+            logging.error(resps)
